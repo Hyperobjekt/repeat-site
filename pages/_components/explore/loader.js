4d46def60e6c0957690ee8690b006b808197c1c4
Uri Maxima <uri@maxima.ninja>
2021-09-25 10:50:52 -0400

    rexolv - #80 - Add "no matching data found" when data absent

--- pages/_components/explore/loader.js
+++ pages/_components/explore/loader.js
@@ -27,17 +27,25 @@ const ExploreLoader = () => {
     return active ? `inline-block rounded border-2 border-transparent text-sm mb-3 mr-3 px-3 py-1 bg-repeat-${color} text-white` : `inline-block rounded text-sm mb-3 mr-3 px-3 py-1 border-2 border-repeat-${color} text-repeat-${color} text-white`;
   };
 
-  
   return (
     <div className="">
       <h2 className="text-repeat-teal text-3xl font-bold mb-3">Examine the Data</h2>
       <p className="text-repeat-dark">Maecenas efficitur dolor. Donec gravida dolor quis dignissim elementum.</p>
       <p className="text-repeat-dark pt-8">Compare by</p>
-      
+
       <ExploreFilters filters={filters} setFilterClasses={setFilterClasses} policy={policy} />
-      <div id="tableContainer" className="overflow-auto">
-        {filters.comparison === "benchmark" ? <ExploreBenchmark tableData={scenarios} /> : <ExploreTimeseries tableData={scenarios} />}
-      </div>
+      {[...scenarios].map((e) => e.values).flat().length ? (
+        <div id="tableContainer" className="overflow-auto">
+          {filters.comparison === "benchmark" ? <ExploreBenchmark tableData={scenarios} /> : <ExploreTimeseries tableData={scenarios} />}
+        </div>
+      ) : (
+        <div className="w-full text-center py-10 px-20">
+          <div className="px-10 py-24 w-2/3 bg-repeat-light-blue m-auto rounded-xl">
+            <h2 className="text-2xl text-repeat">Sorry! No matching data found.</h2>
+            <h4 className="text-xl text-repeat-dark">Adjust the filters and try again.</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
