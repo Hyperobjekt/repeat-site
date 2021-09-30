205aa26d2a4c35d8b2eef42c4fbd8bc442175ad7
Uri Maxima <uri@maxima.ninja>
2021-09-29 23:08:43 -0400

    testing carousel in prod

--- pages/_components/global/image-carousel.js
+++ pages/_components/global/image-carousel.js
@@ -15,19 +15,27 @@ const ImageCarousel = ({ slides }) => {
 
   if (!slides?.length) return null;
   return (
-    <div className="relative">
-      <div className="absolute z-10 left-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "35%" }} onClick={panLeft}></div>
-      <div className="absolute z-10 right-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "35%" }} onClick={panRight}></div>
-      <div className="block whitespace-nowrap overflow-auto w-full no-scrollbar" id="slides-wrapper">
-        {slides.map((slide, i) => (
-          <div className="inline-block px-5 w-1/3  transition-all duration-500 ease-in-out" key={i} style={{ transform: `translateX(-${position * (100 / slides.length)}%)` }}>
-            <div className="block">
-              <img className="w-full" src={slide.image} />
+    <div className="relative overflow-x-hidden">
+      <div className="absolute z-10 left-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={panLeft}></div>
+      <div className="absolute z-10 right-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={panRight}></div>
+      <div className="relative h-72" id="slides">
+        <div
+          className="flex absolute no-scrollbar transition-all duration-500 ease-in-out"
+          id="slides-wrapper"
+          style={{
+            transform: `translateX(-${position * (100 / slides.length)}%)`,
+          }}
+        >
+          {slides.map((slide, i) => (
+            <div className="flex-1 px-5" style={{ minWidth: "275px" }} key={i}>
+              <div className="block">
+                <img className="w-full" src={slide.image} />
+              </div>
+              <div className="block">{slide.caption}</div>
+              <div className="block">{slide.subcaption}</div>
             </div>
-            <div className="block">{slide.caption}</div>
-            <div className="block">{slide.subcaption}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
