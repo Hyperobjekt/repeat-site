9a070d9969c4319bd3213f1317fad7b314ac1333
Uri Maxima <uri@maxima.ninja>
2021-10-10 12:10:35 -0400

    prevent over scrolling

--- pages/_components/global/image-carousel.js
+++ pages/_components/global/image-carousel.js
@@ -2,22 +2,26 @@ import React, { useState, useEffect } from "react";
 
 const ImageCarousel = ({ slides }) => {
   const [position, setPosition] = useState(0);
-  useEffect(() => {}, []);
+  const [tileWidth, setTileWidth] = useState(null);
+  useEffect(() => {
+    let slidesContainer = document.getElementById("slides");
+    setTileWidth(slidesContainer.offsetWidth / 3);
+  }, []);
 
-  const panRight = () => {
-    let newPosition = position === slides.length ? 0 : position + 1;
+  const next = () => {
+    let newPosition = position === slides.length - 3 ? slides.length - 3 : position + 1;
     setPosition(newPosition);
   };
-  const panLeft = () => {
-    let newPosition = position === 0 ? slides.length : position - 1;
+  const previous = () => {
+    let newPosition = position === 0 ? 0 : position - 1;
     setPosition(newPosition);
   };
 
   if (!slides?.length) return null;
   return (
     <div className="relative overflow-x-hidden">
-      <div className="absolute z-10 left-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={panLeft}></div>
-      <div className="absolute z-10 right-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={panRight}></div>
+      <div className="absolute z-10 left-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={previous}></div>
+      <div className="absolute z-10 right-10 w-10 h-10 bg-repeat-light rounded-full shadow-md" style={{ top: "25%" }} onClick={next}></div>
       <div className="relative h-72" id="slides">
         <div
           className="flex absolute no-scrollbar transition-all duration-500 ease-in-out"
@@ -27,7 +31,7 @@ const ImageCarousel = ({ slides }) => {
           }}
         >
           {slides.map((slide, i) => (
-            <div className="flex-1 px-5" style={{ minWidth: "275px" }} key={i}>
+            <div className="flex-1 px-5" style={{ minWidth: `${tileWidth}px` }} key={i}>
               <div className="block">
                 <img className="w-full" src={slide.image} />
               </div>
