892b500318bc6c4a8e8355c858548a6c42c2adac
james-minton <33635008+james-minton@users.noreply.github.com>
2021-10-11 11:58:17 -0400

    Chore(1.0): add favicon

--- pages/_components/global/head.js
+++ pages/_components/global/head.js
@@ -12,7 +12,14 @@ const RepeatHead = () => {
       <meta name="author" content="princeton.edu" />
       <title>Repeat </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" type="image/x-icon" href="/images/branding/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/images/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
       <link rel="stylesheet" href="https://use.typekit.net/vyu0xdh.css"></link>
       <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
