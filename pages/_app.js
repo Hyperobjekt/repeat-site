c3148daf5c16fa743317fdef1e858ec675bdea7c
James Minton <james-minton@github.com>
2021-10-20 14:53:46 -0400

    Bump next version and add GA

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Provider as ReduxProvider } from "react-redux";
 import Router from "next/router";
 import NProgress from "nprogress"; //nprogress module
 import SimpleReactLightbox from "simple-react-lightbox";
+import Script from "next/script";
 
 import { useStore } from "../redux/store.config";
 import "../styles/globals.css";
@@ -15,11 +16,28 @@ Router.events.on("routeChangeError", () => NProgress.done());
 function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
   return (
+    <>
+  <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-7KEFHMCX9L`}
+    />
+    <Script strategy="lazyOnload">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7KEFHMCX9L', {
+            page_path: window.location.pathname,
+          });
+      `}
+    </Script>
+    
     <ReduxProvider store={store}>
       <SimpleReactLightbox>
         <Component {...pageProps} />
       </SimpleReactLightbox>
     </ReduxProvider>
+    </>
   );
 }
 
