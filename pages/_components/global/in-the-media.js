32fb539bca6b9a6246e051f417464c0b0395fbaf
James Minton <james-minton@github.com>
2021-11-11 15:28:54 -0500

    Update media items and style

--- pages/_components/global/in-the-media.js
+++ pages/_components/global/in-the-media.js
@@ -7,29 +7,29 @@ const InTheMedia = () => {
         <h3 className="font-bold text-md uppercase border-b-4 border-repeat-black text-repeat-black">In the Media</h3>
         <div className="grid pt-6 grid-cols-1 md:grid-cols-3 gap-4 place-content-center">
           <div className="block">
-            <div className="h-44 bg-media-vox bg-center bg-cover flex items-center">
+            <a href="https://www.vox.com/22738414/democrats-manchin-climate-reconciliation-plan-b" target="_blank" rel="noopener noreferrer" className="h-44 bg-media-vox bg-center bg-cover flex items-center">
               <div className="w-full text-center">
-                <img className="m-auto max-h-20 max-w-5/7" src="images/vox-logo.svg" alt="" />
+                <img className="m-auto max-h-20 max-w-5/7" src="../images/vox-logo.svg" alt="" />
               </div>
-            </div>
+            </a>
             <div className="pt-2 text-sm font-semibold text-repeat-black">
-              E-Commerce Mega-Warehouses, a Smog Source, Faces New Pollution Regulations
+            Biden’s Plan B for the climate crisis, explained
             </div>
           </div>
 
           <div className="block">
-            <div className="h-44 bg-media-washington-post bg-center bg-cover flex items-center">
+            <a href="https://www.washingtonpost.com/climate-environment/2021/10/28/climate-biden-build-back-better/" target="_blank" rel="noopener noreferrer"  className="h-44 bg-media-washington-post bg-center bg-cover flex items-center">
               <div className="w-full text-center">
-                <img className="m-auto max-h-20 max-w-5/7" src="images/washpo-logo.svg" alt="" />
+                <img className="m-auto max-h-20 max-w-5/7" src="../images/washpo-logo.svg" alt="" />
               </div>
-            </div>
-            <div className="pt-2 text-sm font-semibold text-repeat-black">How Biden Could Deliver Climate Goals</div>
+            </a>
+            <div className="pt-2 text-sm font-semibold text-repeat-black">New budget deal marks the biggest climate investment in U.S. history</div>
           </div>
 
           <div className="block">
             <div className="h-44 bg-media-new-york-times bg-center bg-cover flex items-center">
               <div className="w-full text-center">
-                <img className="m-auto max-h-20 max-w-5/7" src="images/nyt-logo.svg" alt="" />
+                <img className="m-auto max-h-20 max-w-5/7" src="../images/nyt-logo.svg" alt="" />
               </div>
             </div>
             <div className="pt-2 text-sm font-semibold text-repeat-black">Tracking Biden’s Environmental Actions</div>
