4d58d86150fa9d4137a04f7f147c1e5b30967b5f
Corey Tegeler <coreytegeler@gmail.com>
2021-11-11 11:22:52 -0500

    Import antd CSS globally

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import SimpleReactLightbox from "simple-react-lightbox";
 
 import { useStore } from "../redux/store.config";
 import "../styles/globals.css";
+import "antd/dist/antd.css";
 
 //Binding events.
 Router.events.on("routeChangeStart", () => NProgress.start());
